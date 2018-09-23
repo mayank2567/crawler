e590af57ce78b473afeda99cdbf4d6c653251926
Mayank <mayank.chandel@bungeetech.com>
2018-09-23 15:45:47 +0530

    watch youtube

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const crawl = async () => {
   try {
     var body = {
       location: 'all',
-      pickVendor: 'aws'
+      pickVendor: 'gcp'
     };
     const res = await fetch('http://server.e3sfm2eh4k.us-east-1.elasticbeanstalk.com/getRandomDCSProxy', {
       method: 'POST',
@@ -18,38 +18,38 @@ const crawl = async () => {
     proxyIP = json.split('-')[1];
     console.log("proxyIP ", proxyIP);
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       args: [
         `--proxy-server=${proxyIP}:8001`,
         `--ignore-certificate-errors`
       ]
     });
     const page = await browser.newPage();
-    await page.goto('http://amityresult.blogspot.com/2018/01/amity-results-admit-card.html');
-    await page.waitFor(1000);
+    await page.goto('https://www.youtube.com/watch?v=VHZAcDHv2k0');
+    await page.waitFor(31000);
     let bodyHTML = await page.evaluate(() => document.body.innerHTML);
     const $ = cheerio.load(bodyHTML)
     let pages = collectInternalLinks($);
-    pages.map(async (url,index) => {
-      try {
-        if (index == pages.length -1) {
-          const subBrowser = await puppeteer.launch({
-            headless: false,
-            args: [
-              `--proxy-server=${proxyIP}:8001`,
-              `--ignore-certificate-errors`
-            ]
-          });
-          const page = await subBrowser.newPage();
-          console.log("page ", url);
-          await page.goto(url);
-          await page.waitFor(1000);
-          await subBrowser.close();
-        }
-      } catch (err) {
-        console.log("err ", err);
-      }
-    })
+    // pages.map(async (url,index) => {
+    //   try {
+    //     if (index == pages.length -1) {
+    //       const subBrowser = await puppeteer.launch({
+    //         headless: false,
+    //         args: [
+    //           `--proxy-server=${proxyIP}:8001`,
+    //           `--ignore-certificate-errors`
+    //         ]
+    //       });
+    //       const page = await subBrowser.newPage();
+    //       console.log("page ", url);
+    //       await page.goto(url);
+    //       await page.waitFor(1000);
+    //       await subBrowser.close();
+    //     }
+    //   } catch (err) {
+    //     console.log("err ", err);
+    //   }
+    // })
     await browser.close();
   } catch (err) {
     console.log("err ", err);
